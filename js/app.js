482d84a3962565c1bf9b17051c9b931ea406b2ae
Zaira-Zaira <zairahagabanova59@gmail.com>
2021-02-25 09:22:17 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,92 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', (e)=>{
 
     const linkList = document.querySelectorAll("header nav ul:nth-child(2) li a");
   
+
     for (let i = 0; i < linkList.length; i++) {
-      linkList[i].addEventListener("click", () => {
+      linkList[i].addEventListener("click", (e) => {
         for (let i = 0; i < linkList.length; i++) {
-          linkList[i].classList.remove("darkTheme");
-          linkList[i].classList.add("lightTheme");
+       //  let pressed =  (e.target.getAttribute("aria-current") === "true")
+         if(e.target.getAttribute("aria-current") === "true"){
+          linkList[i].classList.add("redTheme");
+          // linkList[i].classList.add("whiteTheme");
+          e.target.setAttribute("aria-current", "false")
+         }
         }
-        linkList[i].classList.remove("lightTheme");
-        linkList[i].classList.add("darkTheme");
+        linkList[i].classList.remove("redTheme");
+        linkList[i].classList.add("whiteTheme");
       });
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // for (let i = 0; i < linkList.length; i++) {
+    //   linkList[i].addEventListener("click", (e) => {
+    //    for (let i = 0; i < linkList.length; i++) {
+
+    //         // Check to see if the button is pressed
+    //   var pressed = (e.target.getAttribute("aria-current") === "true");
+    //   // Change aria-pressed to the opposite state
+    //   e.target.setAttribute("aria-current", !pressed);
+    //   // toggle the play state of the audio file
+              
+    //   if(pressed){
+    //     linkList[i].classList.remove("redTheme");
+    //     linkList[i].classList.add("whiteTheme");
+    //   }
+    //   else{
+    //     linkList[i].classList.add("redTheme");
+    //     linkList[i].classList.remove("whiteTheme");
+    //   }
+    //     }
+    
+    //     linkList[i].classList.remove("redTheme");
+    //     linkList[i].classList.add("whiteTheme");
   
+    //   });
+    // }
+
+    
+
+    // e.target.addEventListener("click", (e)=>{
+    //   console.log(e.target);
+    //   console.log(e.target.getAttribute("aria-current"));
+
+    //   if(e.target.getAttribute("aria-current" == "true")){
+    //     e.target.setAttribute("aria-current", "false")
+    //   }
+    //   else{
+    //     e.target.setAttribute("aria-current", "true")
+    //   }
+    // })
+
+
+    function toggleButton(element) {
+      // Check to see if the button is pressed
+      var pressed = (element.target.getAttribute("aria-current") === "true");
+      // Change aria-pressed to the opposite state
+      element.target.setAttribute("aria-current", !pressed);
+      // toggle the play state of the audio file
+     
+    }
+        
   })
