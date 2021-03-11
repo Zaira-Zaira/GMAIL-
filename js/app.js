8ddb30f3265d005d3c7e14e9b95cf16746ff61d5
Zaira-Zaira <zairahagabanova59@gmail.com>
2021-03-11 14:07:17 +0100

    connexion js update

--- js/app.js
+++ js/app.js
@@ -1,30 +1,50 @@
 document.addEventListener('DOMContentLoaded', (e)=>{
   e.preventDefault()
-   console.log("loaded")
     const linkList = document.querySelectorAll("header nav ul:nth-child(2) li a");
     const submitBtn = document.getElementsByTagName("input")[4]
 
-    //create virtual element warning 
+    for (let i = 0; i < linkList.length; i++) {
+      linkList[i].addEventListener("click", () => {
+        for (let i = 0; i < linkList.length; i++) {
+          linkList[i].classList.remove("redTheme");
+          linkList[i].classList.add("lightTheme");
+        }
+        linkList[i].classList.remove("lightTheme");
+        linkList[i].classList.add("redTheme");
+      });
+    }
 
     let parent = document.querySelector("#form");
     let element = document.createElement('p')
     parent.prepend(element)
+
+    let checkmail =/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/
+    let checkPassword =  /^[a-zA-Z!"#\$%&'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~]{6,30}$/
 
     const User = ({
       firstname: document.getElementsByTagName("input")[0],
       lastname: document.getElementsByTagName("input")[1],
       email: document.getElementsByTagName("input")[2],
       password: document.getElementsByTagName("input")[3],
-
+      userdata: [],
           userDataControll(){
-            if(!this.firstname.value || !this.lastname.value || !this.email.value || !this.password.value){
-              element.innerText="Tous les champs sont obligatoires!!!"
-              element.classList.add("warning")
-            }
-            else{
+            if(this.firstname.value && this.lastname.value && this.email.value && this.password.value){
               document.location.assign("connexion.html")
               localStorage.setItem('User', this.firstname.value)
               sessionStorage.setItem('key', 'user')
+              sessionStorage.setItem('firstname', this.firstname.value)
+              sessionStorage.setItem('email', this.email.value)
+              sessionStorage.setItem('password', this.password.value)
+
+              this.userdata.push(this.email.value, this.password)
+              localStorage.setItem('formdata', this.userdata)
+            }
+            else if(!this.firstname.value || !this.lastname.value || !this.email.value || !this.password.value){
+              element.innerText="Tous les champs sont obligatoires!!!"
+              element.classList.add("warning")
+            }
+            else if(checkmail.exec(this.email.value && checkPassword.exec(this.password.value))){
+                    
             }
           }
     })
--- js/connexion.js
+++ js/connexion.js
@@ -1,13 +1,61 @@
 document.addEventListener('DOMContentLoaded', ()=>{
      
+     
     let target = document.querySelector('h1')
     let userData = localStorage.getItem("User")
     target.innerText+=` ${userData}`
     let SignInBtn = document.querySelector('.signIn')
+    let formdata = localStorage.getItem('formdata')
+    let submit =   document.getElementsByTagName('input')[2]
 
+    
     SignInBtn.addEventListener("click", ()=>{
         document.location.assign("index.html")
         localStorage.clear()
     })
+
+
+    let parent = document.querySelector("#form");
+    let element = document.createElement('p')
+    parent.prepend(element)
+
+
+
+    const checkLoginPassword = ({
+         login: document.getElementsByTagName('input')[0],
+         password: document.getElementsByTagName('input')[1],
+         formdata: localStorage.getItem("formdata"),
+
+
+         firstname = sessionStorage.getItem('firstname'),
+         email = sessionStorage.getItem('email'),
+         passwordUser = sessionStorage.getItem('password'),
+
+         check(){
+              if(this.login.value == this.firstname || this.login.value == this.email){
+                    document.location.assign("userAccount.html")
+              }
+              else if(!this.login.value || !this.password.value  ){
+                element.innerText += "Veuillez renseigner tous les champs"
+                element.classList.add("warning")
+              }
+              else if(this.login.value != this.firstname || this.login.value != this.email){
+                 element.innerText += "Votre login est incorrect"
+                 element.classList.add("warning")
+             }
+             else if(this.password.value != this.passwordUser){
+                 element.innerText += "Votre mot de passe est incorrect"
+                 element.classList.add("warning")
+             }
+         }
+    })
+
+
+     submit.addEventListener('click', (e)=>{
+        e.preventDefault()
+        checkLoginPassword.check()
+     })
+
+
 
 })
